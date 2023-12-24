465262ed69985a6246b7ab2545edc8895f1f81a6
AntonIlyasov <ilyasov.toni.97@mail.ru>
2023-12-24 20:47:22 +0300

    first commit

---
+++ build/encoder/CMakeFiles/encoder.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/anton202/encoder_ws/devel/lib/encoder/encoder"
+  "/home/anton202/encoder_ws/devel/lib/encoder/encoder.pdb"
+  "CMakeFiles/encoder.dir/src/encoder.cpp.o"
+  "CMakeFiles/encoder.dir/src/serialportreader.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/encoder.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/encoder/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/anton202/encoder_ws/src/encoder
+# Build directory: /home/anton202/encoder_ws/build/encoder
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
